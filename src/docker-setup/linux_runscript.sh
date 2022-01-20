db318206350a934c483459a1d46734ecb29fe501
Dai Ngo Tan <ngotandai95@gmail.com>
2022-01-20 16:48:25 -0600

    update

--- src/docker-setup/linux_runscript.sh
+++ src/docker-setup/linux_runscript.sh
@@ -1,5 +1,8 @@
+docker build -f .\Dockerfile_App . --no-cache
+docker build -f .\Dockerfile_MySQL . --no-cache
+docker build -f .\Dockerfile_Mongo . --no-cache
+docker build -f .\Dockerfile_RabbitMQ . --no-cache
 docker-compose up -d itemsdb transactionsdb auctionsdb usersdb watchlistdb messagesmongo messagesrabbitmq
-#docker start itemsdb transactionsdb auctionsdb usersdb watchlistdb messagesmongo messagesrabbitmq
 sleep 120
 docker-compose up items transactions auctions users watchlist frontend
 
